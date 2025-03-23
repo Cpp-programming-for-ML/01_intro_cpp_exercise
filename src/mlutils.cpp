0db2ed2e5ec578ce42f8b323fc389f010da9a250
Lokesh Veeramacheneni <veeramacheneni.lokesh@proton.me>
2025-03-23 16:55:27 +0100

    Update mlutils.cpp

--- src/mlutils.cpp
+++ src/mlutils.cpp
@@ -46,5 +46,18 @@ double cost(twod_array gt, twod_array h){
  */
 void compute_gradients(twod_array w_2, twod_array w_1, twod_array bias, twod_array x, twod_array y,
     twod_array& grad_w2, twod_array& grad_w1, twod_array& grad_bias){
-    // TODO: Compute gradients
+    twod_array h_0 = matadd(matmul(w_1, x), bias);
+    twod_array sigm_h_0 = sigmoid(h_0);
+    twod_array h1 = matmul(w_2, sigm_h_0);
+    twod_array p = matsub(y, h1);
+    
+    grad_w2 = matmul(p, mattranspose(sigm_h_0));
+    grad_w2 = ele_product(grad_w2, -1.0);
+
+    twod_array sigm_deriv = hadamard_product(sigm_h_0, ele_subtract(sigm_h_0, -1.0));
+    twod_array p1 = matmul(mattranspose(w_2), p);
+    twod_array deriv_interim = hadamard_product(p1, sigm_deriv);
+    grad_w1 = matmul(deriv_interim, mattranspose(x));
+    grad_w1 = ele_product(grad_w1, -1.0);
+    grad_bias = ele_product(deriv_interim, -1.0);
 }
